bedf37d974b75c76f6e621c449948faf5e36c39b
JC Sindo <digitaljoni@gmail.com>
2020-05-01 02:03:42 +0800

    1 - initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CritterpediaApp());
+
+class CritterpediaApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Critterpedia',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Critterpedia',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
