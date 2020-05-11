956bf5ea7c1bbb145a0379d1a9b37cd9169d0f86
JC Sindo <digitaljoni@gmail.com>
2020-05-11 14:19:40 +0800

    display all fish in listview

--- lib/models/fish_model.dart
+++ lib/models/fish_model.dart
@@ -10,3 +10,19 @@ class Fish {
         name = json['name']['name-en'],
         price = json['price'];
 }
+
+class AllFish {
+  final Map<String, Fish> fishMap;
+
+  AllFish(this.fishMap);
+
+  AllFish.fromJson(Map<String, dynamic> json)
+      : fishMap = json.map(
+          (String k, dynamic v) => MapEntry(
+            k,
+            Fish.fromJson(v),
+          ),
+        );
+
+  List<Fish> get getList => fishMap.values.toList();
+}
